720c484921a5a7298b845484cc80803b17caea73
Michael Ryan Rosencrans <mikerosencransjr@gmail.com>
2024-03-04 09:33:34 -0500

    APIcallsReact

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,29 @@
+import React from "react"
 
-import React from 'react';
-import BadgeForm from './components/BadgeForm.jsx';
-import Badge from './components/Badge.jsx';
-import './App.css'
-
-function App() {
-  const [badgeInfo, setBadgeInfo] = React.useState({ firstName: '', lastName: '', email: '', phone: '', placeOfBirth: '', favoriteFood: '' });
-  const [badges, setBadges] = React.useState([]);
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setBadgeInfo(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (validateInputs()) {
-      setBadges(prevState => [...prevState, badgeInfo]);
-      setBadgeInfo({ firstName: '', lastName: '', email: '', phone: '', placeOfBirth: '', favoriteFood: '' });
-    } else {
-      alert('Please fill out all fields.');
-    }
-  };
-
-  const validateInputs = () => {
-    return Object.values(badgeInfo).every(value => value.trim().length > 0);
-  };
-
-  return (
-    <div className="container">
-      <BadgeForm
-        badgeInfo={badgeInfo}
-        onInputChange={handleInputChange}
-        onSubmit={handleSubmit}
-      />
-      {badges.map((badge, index) => (
-        <Badge key={index} badge={badge} />
-      ))}
-    </div>
-  );
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(1)
+    
+    /**
+     * Challenge: Combine `count` with the request URL
+     * so pressing the "Get Next Character" button will
+     * get a new character from the Star Wars API.
+     * Remember: don't forget to consider the dependencies
+     * array!
+     */
+    
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [count])
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
 }
-
-export default App;
