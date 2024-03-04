0fac9090a8fdec4deeb62ed917efa505e56283a4
Michael Ryan Rosencrans <mikerosencransjr@gmail.com>
2024-03-04 09:33:50 -0500

    randomColorAssignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import React from "react"
+import React from 'react';
+import axios from 'axios';
+import './App.css'
 
-export default function App() {
-    const [starWarsData, setStarWarsData] = React.useState({})
-    const [count, setCount] = React.useState(1)
-    
-    /**
-     * Challenge: Combine `count` with the request URL
-     * so pressing the "Get Next Character" button will
-     * get a new character from the Star Wars API.
-     * Remember: don't forget to consider the dependencies
-     * array!
-     */
-    
-    React.useEffect(function() {
-        console.log("Effect ran")
-        fetch(`https://swapi.dev/api/people/${count}`)
-            .then(res => res.json())
-            .then(data => setStarWarsData(data))
-    }, [count])
-    
-    return (
-        <div>
-            <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
-        </div>
-    )
-}
+const RandomColorComponent = () => {
+  const [randomColor, setRandomColor] = React.useState('#ffffff'); // Initial state with white color
+
+  React.useEffect(() => {
+    const fetchRandomColor = async () => {
+      try {
+        const response = await axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`);
+        const color = response.data.colors[0].hex;
+        setRandomColor(color);
+      } catch (error) {
+        console.error('Error fetching random color:', error);
+      }
+    };
+
+    fetchRandomColor();
+  }, []);
+
+  const componentStyle = {
+    backgroundColor: randomColor,
+  };
+
+  return (
+    <div className="random-color-component" style={componentStyle}>
+      <h1>Random Color Component</h1>
+      <p>This components background color is set to a random color fetched from the API.</p>
+    </div>
+  );
+};
+
+export default RandomColorComponent;
